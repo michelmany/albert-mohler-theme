bf687a415545cfabde4ad72594f0c3580d4d90f3
michelmany <hello@michelmany.com>
2023-11-21 17:52:56 +0100

    fix: theme enqueuing styles

--- functions.php
+++ functions.php
@@ -277,3 +277,24 @@ function am_theme_options_page_func()
     }
 }
 add_action('init', 'am_theme_options_page_func');
+
+
+/**
+ * Enqueue Styles properly
+ * @return void
+ */
+function my_theme_enqueue_styles(): void
+{
+
+    $parent_style = 'albert-mohler-style';
+
+//    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style(
+        $parent_style,
+        get_template_directory_uri() . '/assets/scss/main.css',
+        array(),
+        wp_get_theme()->get('Version'),
+
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );

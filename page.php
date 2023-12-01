a509fc6ea6a274db57783e8eaee0f96aa35de884
michelmany <hello@michelmany.com>
2023-12-01 17:15:53 +0100

    fix: style subscribe form

--- page.php
+++ page.php
@@ -43,16 +43,16 @@
                                     </div>
                                 </div>
 								<?php the_content(); ?>
-                                <div class="bottom">
-                                    <div class="sign">
-                                        <img src="<?php bloginfo( 'template_url' ) ?>/assets/images/sign.png"
-                                             width="376" alt="">
-                                    </div>
-                                    <p>I am always glad to hear from readers. Write me using the contact form.
-                                        Follow regular updates on Twitter at @albertmohler.
-                                        Subscribe via email for daily Briefings and more (unsubscribe at any time).
-                                    </p>
-                                </div>
+<!--                                <div class="bottom">-->
+<!--                                    <div class="sign">-->
+<!--                                        <img src="--><?php //bloginfo( 'template_url' ) ?><!--/assets/images/sign.png"-->
+<!--                                             width="376" alt="">-->
+<!--                                    </div>-->
+<!--                                    <p>I am always glad to hear from readers. Write me using the contact form.-->
+<!--                                        Follow regular updates on Twitter at @albertmohler.-->
+<!--                                        Subscribe via email for daily Briefings and more (unsubscribe at any time).-->
+<!--                                    </p>-->
+<!--                                </div>-->
                             </div>
                         </div>
                     </div>

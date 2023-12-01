79a1390bf836c55bb25f19fc25437fd66a68d241
michelmany <hello@michelmany.com>
2023-12-01 19:13:04 +0100

    fix: categories template removing 3 dots

--- category-articles.php
+++ category-articles.php
@@ -123,8 +123,10 @@ $post_all   = new WP_Query( $postar_all );
 									<?php the_title(); ?>
                                 </a>
                             </h2>
-                            <p><?php echo mb_substr( wp_strip_all_tags( get_the_content() ), 0, 250, 'UTF-8' ); ?>
-                                ...</p>
+							<?php if ( ! empty( get_the_content() ) ) : ?>
+                                <p><?php echo mb_substr( wp_strip_all_tags( get_the_content() ), 0, 250, 'UTF-8' ); ?>
+                                    ...</p>
+							<?php endif; ?>
                             <h4 class="date"><?php echo get_the_date( 'F j, Y' ); ?></h4>
                         </div>
                     </div>

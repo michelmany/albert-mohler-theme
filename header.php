0f663de5664ed3f4e04aeb5d12a853a0bd67d68a
michelmany <hello@michelmany.com>
2023-12-04 19:23:10 +0100

    fix: mobile nav menu style

--- header.php
+++ header.php
@@ -160,7 +160,26 @@
                       'menu_class' => 'menu menus',
                     )
                   ); 
-                ?>                
+                ?>
+               <?php
+                    wp_nav_menu(
+                      array(
+                        'container' => 'ul',
+                        'theme_location'=>'top_menu',
+                        'menu_id' => 'menu',
+                        'menu_class' => 'menu menus',
+                      )
+                    );
+                  ?>
+                  <?php
+                    wp_nav_menu(
+                      array(
+                        'container' => 'ul',
+                        'theme_location'=>'donate_menu',
+                        'menu_class' =>'action_list',
+                      )
+                    );
+                  ?>
             </div>
         </div>
     </div>

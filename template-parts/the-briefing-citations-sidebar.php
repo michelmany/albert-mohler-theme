81ac8827eb6dc2c2f5c1b8f5929b71e4ab7ccf6c
michelmany <hello@michelmany.com>
2023-12-11 20:36:02 +0100

    fix: citations sidebar errors

--- template-parts/the-briefing-citations-sidebar.php
+++ template-parts/the-briefing-citations-sidebar.php
@@ -4,16 +4,21 @@ $citations = get_field( 'citations' );
 
 <div class="the-briefing-citations-sidebar sidebar-widget">
     <h2>Documentation and Additional Reading</h2>
-	<?php foreach ( $citations as $index => $citation ) : ?>
-        <div class="mb-5">
-            <p class="text-decoration-underline mb-1">
-                PART <?php echo convert_to_roman( $citation['brefing_segment_to_correspond_to'] ); ?></p>
-            <p class="publication"><?php echo $citation['publication']; ?></p>
-            <p class="mb-0">
-                <a href="<?php echo $citation['uri']; ?>" class="citation-link">
-					<?php echo $citation['title']; ?>
-                </a>
-            </p>
-        </div>
-	<?php endforeach; ?>
+
+	<?php
+	if ( ! empty( $citations ) ) :
+		foreach ( $citations as $index => $citation ) : ?>
+            <div class="mb-5">
+                <p class="text-decoration-underline mb-1">
+                    PART <?php echo convert_to_roman( $citation['brefing_segment_to_correspond_to'] ); ?></p>
+                <p class="publication"><?php echo $citation['publication']; ?></p>
+                <p class="mb-0">
+                    <a href="<?php echo $citation['uri']; ?>" class="citation-link">
+						<?php echo $citation['title']; ?>
+                    </a>
+                </p>
+            </div>
+		<?php endforeach;
+	endif;
+	?>
 </div>
